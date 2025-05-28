3778e8713cc6cbf334905cfe9ed2d6450ac72572
emilythaisa <emytonna@gmail.com>
2025-05-28 11:52:44 -0300

    finish

--- src/Clinica.java
+++ src/Clinica.java
@@ -13,6 +13,14 @@ public class Clinica {
         cl.cnpj = "1111.001-05";
         cl.endereço= "Shopping pier 21 loja 01, andar 04";
         
+        /*
+         * 1- encapsule os atributos
+         * 2- defina construtor ou construtores
+         * 3- crie dois objetos
+         * 4- passe parâmetros para construtores sobrecarregadors
+         * 5- gere método getters
+         * 6- e imprima um relatório do objeto criado
+         */
 
         
 
--- src/Exercicio.java
+++ src/Exercicio.java
@@ -2,12 +2,29 @@ package src;
 
 public class Exercicio {
 
-    int id;
-    String nome;
-    String descricao;
-    String som;
-    int tempoMin;
-    String exemplo;
+    private int id;
+    private String nome;
+    private String descricao;
+    private String som;
+    private int tempoMin;
+    private String exemplo;
+
+    public Exercicio(){
+        
+    }
     
-    
+    public Exercicio(String nome, 
+                     String descricao, 
+                     String som,
+                     int tempoMin,
+                     String exemplo){
+
+            this.nome = nome;
+            this.descricao = descricao;
+            this.som = som;
+            this.tempoMin = tempoMin;
+            this.exemplo = exemplo;
+
+                     
+        }
 }
